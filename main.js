83f2d198a1afc92a13f36ff84cb09a34ab5adc7d
Leonardo Puebla <leonardo38361@gmail.com>
2022-04-18 23:05:48 -0300

    Se agrega slide con javascript

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ const table_1 = document.getElementById("table-1")
 const table_2 = document.getElementById("table-2")
 arrow_right.onclick = function () {
     table_1.style.display = "none"
-    table_2.style.display = "grid"
+    table_2.style.display = "flex"
     arrow_left.style.display = "block"
 }
 arrow_left.onclick = function () {
-    table_1.style.display = "grid"
+    table_1.style.display = "flex"
     table_2.style.display = "none"
     arrow_right.style.display = "block"
 }
